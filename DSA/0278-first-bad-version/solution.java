9e991b0875e02b2bfb0668720006613190ada379
Mayank sethiya <114764579+Mayank-sethiya@users.noreply.github.com>
2025-01-22 17:17:24 +0000

    [commit] - First Bad Version - Runtime - 25 ms (5.19%), Memory - 40.4 MB (99.98%)

---
+++ DSA/0278-first-bad-version/solution.java
@@ -0,0 +1,23 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int strt=1;
+        int end=n;
+        while(strt<=end)
+        {
+           int mid=strt+(end-strt)/2;
+            if(isBadVersion(mid))
+            {
+                end=mid-1;
+        }
+            else{
+                strt=mid+1;
+        }
+       
+    
+    }
+ return strt;   
+}
+}
