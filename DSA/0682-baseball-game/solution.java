6e9592e70cbbc1947113b8ef7c5f0b7def78dfac
Mayank sethiya <114764579+Mayank-sethiya@users.noreply.github.com>
2025-02-12 16:29:13 +0000

    [commit] - Baseball Game - Runtime - 2 ms (96.32%), Memory - 41.7 MB (99.99%)

---
+++ DSA/0682-baseball-game/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+   public int calPoints(String[] operations) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+
+            if (op.equals("C")) {
+
+                stack.pop();
+
+            } else if (op.equals("D")) {
+
+                stack.push(stack.peek() * 2);
+
+            } else if (op.equals("+")) {
+
+                int first = stack.pop();
+
+                int second = stack.peek();
+
+                stack.push(first);
+
+                stack.push(first + second);
+
+            } else {
+
+                stack.push(Integer.parseInt(op));
+
+            }
+
+        }
+
+        int total = 0;
+
+        while (!stack.isEmpty()) {
+
+            total += stack.pop();
+
+        }
+
+        return total;
+
+    }
+}
